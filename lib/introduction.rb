bb56ee7f7b7583a3f21ef030a4058426b9890714
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-05 20:31:00 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
 def introduction("Dan", "Ember.js")
-  puts "Hi, my name is #{name}. and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+  
